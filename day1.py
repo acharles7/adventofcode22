0ef2abe29b97792a901fef99a75ecb19f8dda820
Charles Patel <charles.patel@apkudo.com>
2022-12-05 13:58:32 -0600

    Fix prints

--- day1.py
+++ day1.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
 
-def find_top_calories():
+def find_top_calories() -> int:
     calories = Path("data.txt").read_text().splitlines()
-    elves_calories = set()
-    current_total = 0
+
+    current_total: int = 0
+    elves_calories: set[int] = set()
     for calorie in calories:
         if not calorie:
             elves_calories.add(current_total)
@@ -14,11 +15,12 @@ def find_top_calories():
     return max(elves_calories)
 
 
-def find_topk_calories():
+def find_topk_calories() -> int:
     calories = Path("data.txt").read_text().splitlines()
-    elves_calories = set()
-    k = 3
-    current_total = 0
+
+    k: int = 3
+    current_total: int = 0
+    elves_calories: set[int] = set()
     for calorie in calories:
         if not calorie:
             elves_calories.add(current_total)
@@ -26,7 +28,7 @@ def find_topk_calories():
             continue
         current_total += int(calorie)
     topk_calories = sum(sorted(elves_calories)[-k:])
-    print(topk_calories)
+    return topk_calories
 
 
 print(find_top_calories())
